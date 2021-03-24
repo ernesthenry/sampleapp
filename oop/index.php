c3486160f62e77068a6d208881ec75e4be306799
ernesthenry <henry38ernest@gmail.com>
2021-03-24 09:29:15 -0700

    add static methods

---
+++ oop/index.php
@@ -0,0 +1,56 @@
+class Person{
+    private $name; // can be acccessed  outside the class
+    private $email; // can be acccessed  outside the class
+    public static $ageLimit = 40;
+
+    // constructor
+    function __construct($name, $email){
+        $this->name = $name;
+        $this->email = $email;
+        echo __CLASS__. "created successfully". "<br>";
+    }
+
+    // deconstructor
+    function __deconstruct(){
+        echo __CLASS__. "destroyed<br>";
+    }
+
+    function setName($name){
+        $this->name = $name;
+    }
+
+
+    function getName(){
+        return $this->name. "<br>";
+    }
+}
+
+class Customer extends Person(){
+    private $balance;
+
+    function __construct($name, $email, $balance){
+        parent::__construct($name, $email, $balance);
+        $this->balance = $balance;
+        echo "A new ". __CLASS__. "has been created sucessfully";
+    }
+
+    function setBalance($balance){
+        $this->balance = $balance;
+    }
+
+    function getBalance(){
+        return $this->balance. "<br>";
+    }
+}
+
+<!-- customer1 = new Custoomer("Ernest Kato", "test@gmail.com", 3000);
+echo customer1->getBalance(); -->
+
+Person::$ageLimit;
+
+<!-- person1 = new Person("ernest kato", "test@gmail.com"); -->
+<!-- person1->setName("Ernest Kato"); -->
+<!-- echo person1->getName(); -->
+
+<!-- person1 -> $name = "Ernest Henry";
+echo person1->name; -->
